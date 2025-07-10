7217317ee9e275d5193f0be603ed17cd455716b7
bscheucher <bernhard.scheucher@gmail.com>
2025-07-10 19:16:33 +0200

    add dotenv

--- src/main/java/com/bildungsinsitut/deutschkurse/DeutschkurseApplication.java
+++ src/main/java/com/bildungsinsitut/deutschkurse/DeutschkurseApplication.java
@@ -1,5 +1,6 @@
 package com.bildungsinsitut.deutschkurse;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeutschkurseApplication {
 
 	public static void main(String[] args) {
+		// Load .env file INSIDE the main method
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(DeutschkurseApplication.class, args);
 	}
-
 }
